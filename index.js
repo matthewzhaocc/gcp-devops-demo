64f9afbf57198bb7a1b1b78f31bbdbd5b2f7b854
Matthew Zhao <matthew@mail.matthew-cloud.com>
2021-02-06 16:05:09 -0800

    canary v1

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ app.get("/", (req, res) => {
     res.send("hello mate")
 })
 
+app.get("/canary", (req, res) => {
+    res.send("v1")
+})
 app.listen(port, () => {
     console.log(`listening on :${port}`)
 })
